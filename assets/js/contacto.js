5781e60cd39a3e44680f2a7f79aba4319f31c424
Franco Calvo <Fcalvo33@gmail.com>
2023-02-20 02:25:05 -0300

    Revert "contacto mensaje de alerta"
    This reverts commit 344880bf473d5ed050b24d227c50e7d445b80763.

--- assets/js/contacto.js
+++
@@ -1,7 +0,0 @@
-const $submitBtn = document.getElementById("submitbtn");
-
-$submitBtn.addEventListener("click", function () {
-  alert(
-    "¡Sus datos fueron recibidos con exito! Nos mantendremos en contacto para que no te pierdas nuestras novedades."
-  );
-});
