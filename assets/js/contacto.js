4e69fc630555907bc56759d771c6945298b2ae91
IanFeugeas <ianfabricio1889@gmail.com>
2023-02-20 18:41:28 -0300

    mensaje de alerta incorporado

---
+++ assets/js/contacto.js
@@ -0,0 +1,7 @@
+const $submitBtn = document.getElementById("submitbtn");
+
+$submitBtn.addEventListener("click", function () {
+  alert(
+    "Sus datos fueron recibidos con exito! Nos mantendremos en contacto para que no te pierdas nuestras novedades."
+  );
+});
