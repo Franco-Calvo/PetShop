b8d85a814db6477f8a6e1b1c33568d8427caebfe
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-02-21 02:35:36 -0300

    mensaje cuando el usuario manda el formulario

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -1,7 +1,19 @@
-const $submitBtn = document.getElementById("submitbtn");
 
+const $submitBtn = document.getElementById("submitbtn");
+const $formContainer = document.getElementsByClassName("mainContact");
+const form = document.getElementsByTagName("form")[0];
+
+console.log(form)
 $submitBtn.addEventListener("click", function () {
-  alert(
-    "Sus datos fueron recibidos con exito! Nos mantendremos en contacto para que no te pierdas nuestras novedades."
-  );
+mostrarMensaje();
+form.reset();
 });
+
+function mostrarMensaje() {
+$formContainer[0].innerHTML = `
+<div class="mensaje">
+<h2>¡Gracias por contactarte!</h2>
+<p>Sus datos fueron recibidos con exito! Nos mantendremos en contacto para que no te pierdas nuestras novedades.</p>
+</div>
+`;
+}
